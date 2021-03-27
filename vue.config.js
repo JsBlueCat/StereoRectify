ed512c390a9025f1b111f8a4014dd70df46047fd
jsbluecat <15618561709@163.com>
2021-03-27 19:20:23 +0800

    :rocket: add stereo-camera-lib into the gui project

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pluginOptions:{
+    electronBuilder:{
+      nodeIntegration:true,
+      externals:["stereo_camera_api.node"]
+    }
+  }
 }
